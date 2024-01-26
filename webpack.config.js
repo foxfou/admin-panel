1f1b968beafc6b7c404229171fd89bff47310604
Александр <sasha@3davinci.ru>
2024-01-26 17:22:30 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    watchFiles: ["src/**/*"],
+    open: true,
+    port: 3000,
+  },
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+     template: path.resolve(__dirname, 'src', 'index.html')
+  })],
+}
